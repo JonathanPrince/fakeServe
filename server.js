17cf1e35e4e0448b120ee469a9f4a9ece8ba88c0
Jonathan Prince <jonathan.prince@gmail.com>
2014-12-04 11:58:52 +0100

    serve index on root path

--- server.js
+++ server.js
@@ -3,3 +3,15 @@
 var express = require('express')
   , faker   = require('faker');
 
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+// serve html with list of endpoints from root
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
+
+app.listen(8000, function(){
+  console.log('Listening on localhost:8000...');
+});
